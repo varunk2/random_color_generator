b01fc8ded9c53cd2ba012e468dec20955e7b0275
Varun Sharma <varunk22110@gmail.com>
2020-04-06 15:26:07 +0530

    Random Color Generator

---
+++ main.js
@@ -0,0 +1,26 @@
+const button = document.querySelector('.generator');
+const color = document.querySelector('.color');
+
+button.addEventListener('click', setColor);
+color.addEventListener('click', copy);
+
+function setColor(){
+    let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+    document.body.style.backgroundColor = randomColor;
+    color.innerHTML = randomColor;
+}
+
+// Copy function
+function copy(){
+    let text = color.innerText;
+    let input = document.createElement('input');
+    input.setAttribute('value', text);
+    document.body.appendChild(input);
+    input.select();
+
+    document.execCommand('copy');
+    document.body.removeChild(input);
+
+    alert('Copied: ' + text);
+}
